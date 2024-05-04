f9e70806cfb7ba10e252c14c005618aa2b2707e6
Mistzin07 <arthurviniciossoaresbezerra@gmail.com>
2024-05-04 12:10:29 -0300

    Primeiro commit

--- pasta 2/main.js
+++
@@ -1,9 +0,0 @@
-let lista1 = ['bom','lider','otimo','sensacional','lindo','facil']
-let i = 0
-setInterval(()=>{
-            document.querySelector('div').innerHTML = loadMesage[i]
-            i++
-            if(i >= lista1.length){
-                i=0
-            }
-        },500)
